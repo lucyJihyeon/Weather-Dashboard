9acb9af9d4089200d59cb84d8967dc15e3df6823
Lucy Kwon <ks02ks222@gmail.com>
2023-12-06 22:11:43 -0800

    created a starting page

---
+++ assets/script.js
@@ -0,0 +1,30 @@
+//var apikeyOW = "bd198fc2c921dcda5323e5669a78656f";
+var searchFormEl = $("#search-form");
+
+function submitHandler(event)   {
+    event.preventDefault();
+
+    var searchInput = $("#search").val();
+    console.log(searchInput);
+    if (!searchInput)   {
+        var enterAgain = $("#enter-again")
+        for (var i = 0; i < enterAgain.length; i ++)    {
+            enterAgain[i].empty();
+        }
+        var please = $("<p>").text("Please enter a city!").css({
+            "color": "white",
+            "text-align":"center",
+        }).attr("id", "enter-again");
+        searchFormEl.append(please);
+        return;
+    }
+    var queryString = "./search-results.html"
+    location.assign(queryString);
+
+}
+
+searchFormEl.on("click", submitHandler);
+
+
+
+
